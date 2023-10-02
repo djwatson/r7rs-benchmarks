61aa35f13fdd54a68cc542b16fe0b08c5f5f0b0c
Dave Watson <dade.watson@gmail.com>
2023-10-02 09:35:01 -0700

    chez: Change chez prelude/postlude, this seems to work better for me?

--- src/Chez-prelude.scm
+++ src/Chez-prelude.scm
@@ -1,4 +1,7 @@
-
+(import (chezscheme))
+(define (current-jiffy) (with-input-from-file "/proc/uptime" read))
+(define (current-second) (with-input-from-file "/proc/uptime" read))
+(define (jiffies-per-second) 1)
 
 
 
